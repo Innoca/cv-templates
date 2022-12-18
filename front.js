330e4abc50d3e81d7541472f304a989e4b880fc7
Innoca <48687109+Innoca@users.noreply.github.com>
2022-12-18 11:19:07 +0200

    updates

---
+++ front.js
@@ -0,0 +1,30 @@
+// get the navigation bar element 
+
+var navBar = document.getElementById("navigation_bar")
+
+// add a click event listener to the navigation bar
+
+navBar.addEventListener("click", function(){
+    //here we remove the current color class from the navBar
+
+    navBar.classList.remove("color_green","color_blue","color_pink","color_orange","color_yellow");
+   
+    // generating a random number between 1 and 5
+
+    var randomNumber = Math.floor(Math.random() * 5) + 1;
+
+    //adding a new color class to the navBar based on the random number
+
+    if(randomNumber === 1) {
+        navBar.classList.add("color_green");
+    } else if (randomNumber === 2) {
+        navBar.classList.add("color_blue");
+    } else if (randomNumber === 3) {
+        navBar.classList.add("color_pink");
+    } else if (randomNumber === 4) {
+        navBar.classList.add("color_orange");
+    } else {
+        navBar.classList.add("color_yellow");
+    }
+}
+)
